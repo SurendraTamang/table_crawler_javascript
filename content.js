4a3f4c6e3aa7b23200ac84f60aa0359187d3b79e
suren40 <72bel240@wrc.edu.np>
2020-01-06 10:03:55 +0545

    new functions and layout added

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ function has started
 */
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request)
+        console.log(request['id'])
         let result = crawling_start(request)
         sendResponse(result)
       
@@ -13,7 +13,15 @@ chrome.runtime.onMessage.addListener(
 /* I am using the xpath for crawling the table data 
 the document.evaluate is used. */
 function crawling_start(xpath){
-    let input = xpath
+// For the creation of request
+console.log("request is initilize")
+const request = new XMLHttpRequest();
+const base_url = "https://4yxdxb3wpl.execute-api.us-east-1.amazonaws.com/upload/json-upload"
+request.open('POST',base_url,true)
+request.setRequestHeader("Content-Type", "application/json");
+let begin_cell = xpath['begin']
+let sheet_ID = xpath['sheet']
+let input = xpath['id'].toString();
 let xpath_input_id = "//table[@id='"+input+"']"
 let xpath_input_class = "//table[@class='"+input+"']"
 let xpath_expression = xpath_input_id+"//tr"+"|"+xpath_input_class+"//tr"
@@ -33,7 +41,8 @@ data = alertText[i].trim().split("\n")
 data_list.push(data)
 }
 //contains all the list of data
-console.log(data_list)
+//console.log(data_list)
+
 //creating the list for putting objects
 details_list = []
 // creating an object
@@ -45,9 +54,12 @@ for (i = 1; i < data_list.length;i++){
     for (j = 0; j<data_list[i].length;j++){
         details[data_list[0][j]] = data_list[i][j]
     }
-    console.log(details)
+   // console.log(details)
     details_list.push(details)
 }
-console.log(details_list)
+console.log("Sending")
+let json_file = {begin:begin_cell,sheet_id:sheet_ID,data:details_list}
+var myJsonString = JSON.stringify(json_file);
+request.send(myJsonString);
 return details_list;
 }
--- popup.js
+++ popup.js
@@ -1,21 +1,23 @@
-console.log("The browser action is working!");
+
 
 document.getElementById("xpath_submit").onclick = sendMessage;
 
 function sendMessage(){
 let msg = document.getElementById("x_path_input").value;
-    console.log(msg)
+let begin_sheet = document.getElementById("begin").value;
+let sheet_id = document.getElementById("sheet_id").value;
+let message = {id:msg,begin:begin_sheet,sheet:sheet_id};
 params = {active: true, currentWindow: true}
     chrome.tabs.query(params, send);
     function send(tabs) {
         
-        chrome.tabs.sendMessage(tabs[0].id, msg,resp)
+        chrome.tabs.sendMessage(tabs[0].id, message,resp)
         
         function resp(response){
             console.log(response);
             var obj = {data:response};
 var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
-
+// Created to JSON file
 var a = document.createElement('a');
 a.href = 'data:' + data;
 a.download = 'data.json';
